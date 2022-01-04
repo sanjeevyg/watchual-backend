eec08f9d7e0cdd966384d03a2f0f9c56951eabe2
sanjeevyg <yogi.sjv@gmail.com>
2022-01-04 02:31:38 -0500

    login route and route to add user added

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const database = knex(config)
 const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 
+const bcrypt = require("bcrypt")
+let jwt = require('jsonwebtoken');
+
 const port = 5000
 
 app.listen(port, () => {console.log(`listenting to ${port}`)})
+
 
 app.get('/', (request, response) => {
     database("users")
@@ -26,19 +30,52 @@ app.get('/', (request, response) => {
     })
 })
 
-app.post("/users", (request, response) => {
+app.post("/users", ( request, response ) => {
+    const { user } = request.body
+
+    bcrypt.hash(user.password, 12)
+        .then(hashed_password => {
+           return database("users")
+                .insert({
+                    username: user.username,
+                    password_hash: hashed_password
+                }) 
+                .returning("*")
+                .then(users => {
+                    const user = users[0]
+                    response.json({ user })
+                }).catch(error => {
+                    response.json({ error: error.message })
+                })
+        }
+    )
+})
+
+app.post("/login", ( request, response ) => {
     const { user } = request.body
 
     database("users")
-    .insert({
-        username: user.username,
-        password_hash: user.password
-    }) 
-    .returning("*")
-    .then(users => {
-        const user = users[0]
-        response.json({ user })
-    }).catch(error => {
-        response.json({ error: error.message })
-    })
+        .where({username: user.username })
+        .first()
+        .then(retrievedUser => {
+            if(!retrievedUser) throw new Error("user not found!")
+
+            return Promise.all([
+                bcrypt.compare(user.password, retrievedUser.password_hash),
+                Promise.resolve(retrievedUser)
+            ]).then(results => {
+                const areSamePasswords = results[0]
+                if(!areSamePasswords) throw new Error("wrong Password!")
+                const user = results[1]
+                const payload = {username: user.username}
+                const secret = "SECRET"
+
+                jwt.sign(payload, secret, (error, token) => {
+                    if(error) throw new Error("Sign in error!")
+                    response.json("it's a match")
+                }).catch(error => {
+                    response.json({message: error.message})
+                })
+            })
+        })
 })
